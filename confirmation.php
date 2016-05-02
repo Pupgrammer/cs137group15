d7083d7c3bf1003969dea5a8c4253562f8478328
Bryan Nham <bryan.nham@yahoo.com>
2016-05-02 15:59:42 -0700

    changed confirmation to just accessing last row in database with no submissions

--- confirmation.php
+++ confirmation.php
@@ -1,48 +1,3 @@
-<?php
-
-require_once 'connection_info.php';
-$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-/*   Prints out the POST array values in order.
-
-foreach ($_POST as $data2)
-{
-    echo "$data2 <br>";
-}
-
-*/
-
-$price = "SELECT price FROM products WHERE products.product_number = " . $_POST['product_number'] . ";";
-$coststatement = $pdo->query($price);
-
-$costarray = $coststatement->fetch(PDO::FETCH_ASSOC);
-$cost = $costarray['price'];
-
-
-$order = "INSERT INTO order_info (order_id, product_number, product_name, quantity, first_name, last_name, email, phone_number, address, zipcode, city, state, shipping_method, credit_card, cost)  
-    VALUES (:order_id, :product_number, :product_name, :quantity, :first_name, :last_name, :email, :phone_number, :address, :zipcode, :city, :state, :shipping_method, :credit_card, :cost)";
-$order_stmt = $pdo->prepare($order);
-$order_stmt->execute(array(
-    ':order_id' => "null",
-    ':product_number' => $_POST['product_number'],
-    ':product_name' => $_POST['friendly_name'],
-    ':quantity' => $_POST['quantity'],
-    ':first_name' => $_POST['firstName'],
-    ':last_name' => $_POST['lastName'],
-    ':email' => $_POST['email'],
-    ':phone_number' => $_POST['phoneNumber'],
-    ':address' => $_POST['streetAddress'],
-    ':zipcode' => $_POST['zipcode'],
-    ':city' => $_POST['city'],
-    ':state' => $_POST['state'],
-    ':shipping_method' => $_POST['shipping'],
-    ':credit_card' => $_POST['creditCard'],
-    ':cost' => $cost,
-));
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,6 +21,9 @@ $order_stmt->execute(array(
   <p> Here is your order information:
   <br>
   <?php
+  require_once 'connection_info.php';
+  $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "SELECT * FROM order_info ORDER BY order_id DESC LIMIT 1";
   $statement = $pdo->query($sql);
   
@@ -85,18 +43,8 @@ $order_stmt->execute(array(
     echo "<br><b>Shipping Method: </b>" . $order['shipping_method'];
     $card = "**** **** **** " . substr($order['credit_card'],-4);
     echo "<br><b>Credit Card: </b>" . $card;
-    $cost *= $order['quantity'];
-    if ($order['shipping_method'] == 'One Day') {
-        $cost += 10;
-    }
-    elseif ($order['shipping_method'] == 'Two Day') {
-        $cost += 5;
-    }
-    echo "<br><b>Total Cost: </b>$" . $cost;
-    $changecost = "UPDATE order_info SET cost=" . $cost . " WHERE order_id=" . $order['order_id'] . ";";
-    $costupdate = $pdo->query($changecost);
+    echo "<br><b>Total Cost: </b>$" . $order['cost'];
   }
-  
   ?>
   </div>
   </p>
