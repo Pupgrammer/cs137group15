fff1f54392dbc2d5ceade8ca42ed8f763e6c5263
thomatn5 <thomatn5@uci.edu>
2016-05-25 17:32:05 -0700

    SubmitOrder.java created for order submission handling. Completely empty as of now.

---
+++ src/SubmitOrder.java
@@ -0,0 +1,16 @@
+
+import javax.servlet.http.HttpServlet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tai
+ */
+public class SubmitOrder extends HttpServlet {
+    
+}
