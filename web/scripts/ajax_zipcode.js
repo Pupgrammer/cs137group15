07e190bbbda4626ec46657581a38dd92c70b2f34
Brian Chipman <namedtuple@gmail.com>
2016-05-26 12:55:33 -0700

    Suggested zip code list is now sorted.  Also some function renaming and minor code cleanup.

--- web/scripts/ajax_zipcode.js
+++ web/scripts/ajax_zipcode.js
@@ -4,19 +4,20 @@
  * Filename: ajax_cityState.js
  */
 
+
 /* Active Functions */
 
 document.getElementById("zipcode").onblur = function () {
     var zipcode = getZipFromInputBox();
     if (zipcode != "") {
         if (getLengthOfNumber(zipcode) == 5) {
-            setTimeout(closeSuggestions, 200);
+            setTimeout(closeZipSuggestions, 200);
             var xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     var cityState = xhr.responseText;
                     var split = cityState.split(',');
-                    changeCityState(split[0], split[1]);
+                    setCityState(split[0], split[1]);
                 }
             };
             xhr.open("GET", "/checkout?event=onblur&zipcode=" + getZipFromInputBox(), true);
@@ -25,10 +26,10 @@ document.getElementById("zipcode").onblur = function () {
         else if (getLengthOfNumber(zipcode) > 5) {
             alert("Zipcode should only contain 5 digits maximum.");
             document.getElementById("zipcode").value = "";
-            closeSuggestions();
+            closeZipSuggestions();
         }
         else {
-            setTimeout(closeSuggestions, 200);
+            setTimeout(closeZipSuggestions, 200);
         }
     }
 };
@@ -40,7 +41,7 @@ document.getElementById("zipcode").onkeyup = function () {
         if (checkDigits != true) {
             alert("Zipcode should only contain digits.");
             document.getElementById("zipcode").value = "";
-            closeSuggestions();
+            closeZipSuggestions();
         }
         else {
             zipcode = fixDigits(zipcode);
@@ -49,6 +50,7 @@ document.getElementById("zipcode").onkeyup = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     var suggestions = xhr.responseText;
                     var split = suggestions.split('|');
+                    split.sort();
                     showZipSuggestions(split);
                 }
             };
@@ -58,26 +60,20 @@ document.getElementById("zipcode").onkeyup = function () {
     }
 };
 
+
 /* Helper Functions */
+
 function getZipFromInputBox() {
     return document.getElementById("zipcode").value;
 }
 
-function changeCityState(city, state) {
+function setCityState(city, state) {
     if (city != "") {  // && state == ""? Works without it, and doesn't work with it.
         document.getElementById("city").value = city;
     }
     if (state != "") {
         document.getElementById("state").value = state;
     }
-}
-
-function getLengthOfNumber(number) {
-    return number.toString().length;
-}
-
-function fixDigits(zipcode) {
-    return String(zipcode + "00000").slice(0, 5);
 }
 
 function showZipSuggestions(split) {
@@ -92,15 +88,23 @@ function showZipSuggestions(split) {
     }
 }
 
-function closeSuggestions() {
+function setZip(zipcode) {
+    closeZipSuggestions();
+    document.getElementById("zipcode").value = zipcode;
+    document.getElementById("zipcode").focus(); // This is to force the server to recalculate the new city/state pair. It will call getCityState.
+    document.getElementById("zipcode").blur();
+}
+
+function closeZipSuggestions() {
     document.getElementById("suggestions").style.backgroundColor = "";
     document.getElementById("suggestions").style.border = "0px"; // Color taken from W3 schools
     document.getElementById("suggestions").innerHTML = "";
 }
 
-function setZip(zipcode) {
-    closeSuggestions();
-    document.getElementById("zipcode").value = zipcode;
-    document.getElementById("zipcode").focus(); // This is to force the server to recalculate the new city/state pair. It will call getCityState.
-    document.getElementById("zipcode").blur();
+function getLengthOfNumber(number) {
+    return number.toString().length;
+}
+
+function fixDigits(zipcode) {
+    return String(zipcode + "00000").slice(0, 5);
 }
