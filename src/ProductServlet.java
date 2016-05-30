6e4d0ccba7e14dfb18287eb18673e7678dc59d38
alexl10 <alexl10@uci.edu>
2016-05-30 15:49:05 -0700

    Update ProductServlet.java
    Collaborated with bryan, added hashmap to count view history, need to figure out a way to add count when different session ID is detected

--- src/ProductServlet.java
+++ src/ProductServlet.java
@@ -5,6 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.Map;
+import java.util.HashMap;
+import javax.servlet.http.HttpSession;
 
 public class ProductServlet extends HttpServlet {
 
@@ -12,7 +15,31 @@ public class ProductServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession(true); 
+        
+        HashMap<Integer, Integer> counter = (HashMap<Integer, Integer>) session.getAttribute("counter");
 
+            if (counter == null)
+            {
+                counter = createNewCounter(session);
+                out.println("Session counter for" + session.getId() + "was empty so created with empty values");
+                out.println("adding count to " + request.getParameter("product_number"));
+                int p = Integer.parseInt(request.getParameter("product_number"));
+                int count = counter.containsKey(p) ? counter.get(p) : 0;
+                counter.put(p, count + 1);
+                out.println("counter for this is :" + count);
+            }
+            else
+            {
+                out.println(" continue adding count to " + request.getParameter("product_number"));
+                int p = Integer.parseInt(request.getParameter("product_number"));
+                int count = counter.containsKey(p) ? counter.get(p) : 0;
+                counter.put(p, count + 1);
+                out.println("counter for this is :" + count);
+            }
+        
+        printProductView(out, session);
+        
         out.println("<!DOCTYPE html>");
         out.println("<html lang=\"en\">");
         out.println("<head>");
@@ -34,6 +61,9 @@ public class ProductServlet extends HttpServlet {
         out.println("</div>");
         out.println("<table class=\"info\">");
 
+        
+        
+        
         try {
             String sql = "SELECT * FROM products WHERE product_number=" + request.getParameter("product_number")  + ";";
             DatabaseResultSet dbrs = new DatabaseResultSet(sql);
@@ -87,10 +117,15 @@ public class ProductServlet extends HttpServlet {
                 out.println("<td class=\"desc\">" + dataRow.get("operating_system") + "</td>");
                 out.println("</tr>");
                 
+                updateViewedProducts(request,session);
+               
+                
                 out.println("<form action=\"checkoutdebug\" method=\"post\">");
                 out.println("<input name=\"addProductToCart\" type=\"hidden\" value=\""+request.getParameter("product_number")+"\">");
                 out.println("<input type=\"submit\" value=\"addProductToCart\"/>");
                 out.println("</form>");
+                
+                
             }
         }
 
@@ -101,6 +136,49 @@ public class ProductServlet extends HttpServlet {
         out.println("</body>");
         out.println("</html>");
 
+    }
+    
+    private void updateViewedProducts(HttpServletRequest request, HttpSession session) {
+        String[] viewed = (String[]) session.getAttribute("products");
+        for (int i = viewed.length-2; i >= 0; i--) {
+            viewed[i+1] = viewed[i];
+        }
+        viewed[0] = request.getParameter("product_number");
+        session.setAttribute("products",viewed);
+    }
+    
+    
+    
+    void printProductView(PrintWriter out, HttpSession session) {
+        out.println("<p> Items previously checked: ");
+        String[] viewed = (String[]) session.getAttribute("products");
+        if (!(viewed[0].equals("0"))) {
+            out.println("<a href=\"product?product_number=" + viewed[0] + "\">" + viewed[0] + "</a>");
+            for (int i = 1; i < viewed.length; i++) {
+                if ((viewed[i].equals("0"))) {
+                    break;
+                }
+                else {
+                  //  out.println(" , " + "<a href=\"product?product_number=" + "\">" + viewed[i] + "</a>");
+                 //  out.println("<img src=\"product?product_number=" + viewed[i] + "\"");
+                } 
+           }
+        }
+        else {
+            out.println("None");
+        }
+        out.println("</p>");
+        
+      
+    }
+    
+        HashMap<Integer, Integer> createNewCounter(HttpSession session) { // Creates a new counter map for the session, and returns it.
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(20);
+        for (int i = 1; i < 10; i++){
+            map.put(i, 0);
+        }
+        session.setAttribute("counter", map);
+        return map;
     }
 
 }
