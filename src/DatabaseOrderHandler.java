98ded17fc2a2819bfc7944df6c19be4eebae4c0c
thomatn5 <thomatn5@uci.edu>
2016-05-30 18:52:36 -0700

    Unfinished progress on DatabaseOrderHandler.java and SubmitOrder.java -- currently working on it.

---
+++ src/DatabaseOrderHandler.java
@@ -0,0 +1,82 @@
+import java.sql.*;
+
+public class DatabaseOrderHandler {
+
+    Connection current;
+    
+    public DatabaseOrderHandler() 
+    {
+        current = null;
+    }
+    
+    public void close()
+    {
+        current = null;
+    }
+    
+    private Connection handleGetConnection()
+    {
+        if (current != null)
+        {
+            return current;
+        }
+        else
+        {
+            // JDBC driver name and database URL
+            final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+            final String SERVER_NAME = ConnectionInfo.SERVER_NAME;
+            final String DB_NAME = ConnectionInfo.DATABASE_NAME;
+
+            //  Database credentials
+            final String USER = ConnectionInfo.USER_NAME;
+            final String PASS = ConnectionInfo.PASSWORD;
+
+            try {
+                // Register JDBC driver
+                Class.forName(JDBC_DRIVER);
+
+                current = DriverManager.getConnection(SERVER_NAME + DB_NAME, USER, PASS);
+            }
+            catch (SQLException | ClassNotFoundException e)
+            {
+                e.printStackTrace();  
+            }
+            return current;
+        }
+    }
+
+    public void executeOrderInfoStatement(String order_id, int product_id, int quantity, double total_cost)
+    {
+        /* OrderInfo Structure:  CREATE TABLE `order_info`
+            (
+            `order_id` varchar(40) NOT NULL,
+            `product_id` int(5) NOT NULL,
+            `product_name` varchar(40) NOT NULL,
+            `quantity` int(3) NOT NULL,
+            `total_cost` float(20) NOT NULL,
+            PRIMARY KEY (order_id, product_id)
+            ) ENGINE=InnoDB;
+        */
+        
+        String orderSQL = "INSERT INTO order_info (order_id, product_id, product_name, quantity, total_cost) VALUES (?, ?, ?, ?, ?);";
+
+        
+        try
+        {
+            Connection connection = handleGetConnection();
+
+            // Execute SQL query
+            PreparedStatement statement = connection.prepareStatement(orderSQL);
+            statement.setString(1, "order_id_test");
+            statement.setInt(2, 123);
+            statement.setString(3, "product_name");
+            statement.setInt(4, 1);
+            statement.setFloat(5, new Float(123.00));
+            statement.executeUpdate();
+        }            
+        catch (SQLException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- src/SubmitOrder.java
+++ src/SubmitOrder.java
@@ -8,22 +8,27 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Iterator;
 import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.sql.*;
 
 public class SubmitOrder extends HttpServlet 
 {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
         PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
         HashMap<String, String> order = organizeOrderInfo(request, request.getParameterNames());
         HashMap<Integer, Integer> cart = (HashMap<Integer, Integer>) request.getSession().getAttribute("cart");
 
+
         printDebugInformation(out, order, cart);
+        executeOrderSQLStatement(session.getId(), order, cart);
     }
     
     HashMap<String, String> organizeOrderInfo(HttpServletRequest request, Enumeration<String> parameterNames) {
@@ -36,11 +41,55 @@ public class SubmitOrder extends HttpServlet
         return result;
     }
     
-    String createOrderSQLStatement(HashMap<Integer, Integer> cart) { // Assumes that cart has at least one element and order is not empty.
-        String sql = "INSERT INTO tables VALUES from products WHERE product_number = ";
-        return sql;
+    private void executeOrderSQLStatement(String session_id, HashMap<String, String> order, HashMap<Integer, Integer> cart) 
+    { // Assumes that cart has at least one element and order is not empty.
+        HashMap<Integer, Double> product_costs = getProductCosts(cart);
+        DatabaseOrderHandler connection = new DatabaseOrderHandler();
+        Iterator iterate = cart.entrySet().iterator();
+        while (iterate.hasNext())
+        {
+            Map.Entry pair = (Map.Entry) iterate.next(); // Key = Product ID, Value = Quantity of Product ID
+            connection.executeOrderInfoStatement(session_id, (int) pair.getKey(), (int) pair.getValue(), product_costs.get(pair.getKey()) * (double) pair.getValue());
+        }
+
     }
     
+    private HashMap<Integer, Double> getProductCosts(HashMap<Integer, Integer> cart)
+    {
+        HashMap<Integer, Double> product_costs = new HashMap<Integer, Double>();
+        DatabaseResultSet database_costs = new DatabaseResultSet(createProductSQLStatement(cart));
+        ResultSet result = database_costs.getResultSet();
+        try
+        {
+            while (result.next())
+            {
+                product_costs.put(result.getInt("product_id"), result.getDouble("price"));
+            }
+        }
+        catch (SQLException e ) 
+        {
+            e.printStackTrace();
+        }
+        return product_costs;
+    }
+    
+    String createProductSQLStatement(HashMap<Integer, Integer> cart) {
+        int counter = 0;
+        String sql = "SELECT product_number AS product_id, price from products WHERE product_number = ";
+        for (Integer product_id : cart.keySet())
+        {
+            if (counter == 0)
+            {
+                sql = sql + product_id;
+                counter = counter + 1;
+            }
+            else
+            {
+                sql = sql + " OR product_number = " + product_id;
+            }
+        }
+        return sql;
+    }
     void printDebugInformation(PrintWriter out,  HashMap<String, String> order, HashMap<Integer, Integer> cart)
     {
         for (Map.Entry<String, String> entry : order.entrySet())
